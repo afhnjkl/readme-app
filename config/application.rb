d238a57e2d00ef62aa5bc2c1b70bcc337815b7d1
afhnjkl <hmktrs3@gmail.com>
2020-02-18 18:28:48 +0900

    add_message

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(*Rails.groups)
 
 module ReadmeApp
   class Application < Rails::Application
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+      %Q(#{html_tag}).html_safe
+    end
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
