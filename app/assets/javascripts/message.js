6a8d4d915b9f6f52a487414c0d43ee24517cd8db
afhnjkl <hmktrs3@gmail.com>
2020-02-23 20:40:22 +0900

    修正しました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -52,7 +52,6 @@ $(function(){
     contentType: false
   })
    .done(function(data){
-     console.log(html)
      var html = buildHTML(data);
      $('.messages').append(html);
      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
