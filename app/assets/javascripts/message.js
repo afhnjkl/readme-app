935ef348fc9beff73662073834992b99d79524aa
afhnjkl <hmktrs3@gmail.com>
2020-02-25 17:30:12 +0900

    自動更新

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,8 +2,7 @@ $(function(){
   function buildHTML(message){
    if ( message.image ) {
      var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="upper-message">
+      ` <div class="upper-message" data-message-id=${message.id}>
            <div class="upper-message__user-name">
              ${message.user_name}
            </div>
@@ -19,10 +18,10 @@ $(function(){
          <img src=${message.image} >
        </div>`
      return html;
-   } else {
+   } 
+   else {
      var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="upper-message">
+      `<div class="upper-message" data-message-id=${message.id}>
            <div class="upper-message__user-name">
              ${message.user_name}
            </div>
@@ -45,13 +44,14 @@ $(function(){
   var url = $(this).attr('action')
   $.ajax({
     url: url,
-    type: "POST",
+    type: "post",
     data: formData,
     dataType: 'json',
     processData: false,
     contentType: false
   })
    .done(function(data){
+    
      var html = buildHTML(data);
      $('.messages').append(html);
      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
@@ -61,9 +61,42 @@ $(function(){
     alert("メッセージ送信に失敗しました");
    })
    .always(function() {
-    form_submit.prop('disabled', false);
+    $('.form_submit').prop('disabled', false);
   });
-
- })
 });
 
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.upper-message:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+    });
+    
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
+
+});
