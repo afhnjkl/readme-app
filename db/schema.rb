818881fe124194ed4cd420093c46dd4634625cf5
afhnjkl <hmktrs3@gmail.com>
2020-02-26 21:48:50 +0900

    update table

--- db/schema.rb
+++ db/schema.rb
@@ -43,11 +43,7 @@ ActiveRecord::Schema.define(version: 2020_02_16_051232) do
     t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "reset_password_token", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
